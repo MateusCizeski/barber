491e8e111c544bcd87a158046bbc2acae3b902db
mateu <mateuscizeski@gmail.com>
2025-02-12 21:07:42 -0300

    funcionalidade do haircut

---
+++ src/controllers/haircut/CreateHairCutController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { CreateHaircutService } from "../../services/haircut/CreateHairCutService";
+
+class CreateHairCutController {
+    async handle(request: Request, response: Response) {
+        const { name, price } = request.body;
+        const user_id = request.user_id;
+
+        const createHaircutService = new CreateHaircutService();
+        const haircut = await createHaircutService.execute({ user_id, name, price });
+
+        return response.json(haircut);
+    }
+}
+
+export { CreateHairCutController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from "express";
 
+//USER
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { UpdateUserController } from "./controllers/user/UpadateUserController";
+
+//HAIRCUT
+import { CreateHairCutController } from "./controllers/haircut/CreateHairCutController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -14,5 +18,8 @@ router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 router.put('/users', new UpdateUserController().handle);
+
+// --- ROTAS HAIRCUT ---
+router.post('haircut', isAuthenticated, new CreateHairCutController().handle);
 
 export { router };
